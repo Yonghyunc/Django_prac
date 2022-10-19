d84619a001cbbd006a357b38cf292b2024088e5e
yonghyunc <chyh9218@gmail.com>
2022-10-19 21:51:46 +0900

    Insta Clone Coding 1, 2

--- insta/posts/views.py
+++ insta/posts/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import PostForm
+from .models import Post
 
 # Create your views here.
-
 def index(request):
     return render(request, 'posts/index.html')
 
-    
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+    else:
+        form = PostForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'posts/create.html', context)
+
+
+def post(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/post.html', context)
+
+
+def delete(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    post.delete()
+    return redirect('posts:index')
